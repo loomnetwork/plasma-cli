d5f17bb0201c28718b7a5084cab621679e26c404
Georgios Konstantopoulos <me@gakonst.com>
2018-10-06 19:11:16 +0300

    Extract prompt functions to its own file

--- src/modules/repl.ts
+++ src/modules/repl.ts
@@ -1,10 +1,9 @@
-import Web3 from 'web3'
-import { ERC20, ERC721, createEntity } from './config'
-import repl from 'repl'
 import BN from 'bn.js'
-import { increaseTime } from './ganache-helpers'
 import { PlasmaDB } from 'loom-js'
-import { IDatabaseCoin } from 'loom-js/dist/plasma-cash/db'
+import repl from 'repl'
+import Web3 from 'web3'
+import { createEntity, ERC20, ERC721 } from './config'
+import { User } from './user'
 
 const transform = require('./repl-utils/transform')
 
@@ -20,16 +19,16 @@ export function startCLI(
   const provider = new Web3.providers.WebsocketProvider(web3Endpoint)
   const web3 = new Web3(provider)
   const database = new PlasmaDB(web3Endpoint, dappchainEndpoint, rootChain, privateKey)
-  const user = createEntity(
+  const entity = createEntity(
     web3,
     web3.utils.toChecksumAddress(rootChain),
     dappchainEndpoint,
     privateKey,
     database
   )
-  const myERC721 = (addr: string) => ERC721(web3, addr, user.ethAccount)
-  const myERC20 = (addr: string) => ERC20(web3, addr, user.ethAccount)
-
+  const ERC721At = (addr: string) => ERC721(web3, addr, entity.ethAccount)
+  const ERC20At = (addr: string) => ERC20(web3, addr, entity.ethAccount)
+  const token = ERC721At(erc721!).instance
   // Create the REPL
   const prompt = repl.start('$ ')
 
@@ -42,89 +41,32 @@ export function startCLI(
     selfPrivate: privateKey
   }
 
-  // Various objects
+  // Low level prompt objects
   prompt.context.BN = BN
-  prompt.context.plasma = user.plasmaCashContract
+  prompt.context.plasma = entity.plasmaCashContract
   prompt.context.web3 = web3
   prompt.context.eth = web3.eth
-  prompt.context.dappchain = user
-  prompt.context.ERC20 = myERC20
-  prompt.context.ERC721 = myERC721
+  prompt.context.dappchain = entity
+  prompt.context.ERC20 = ERC20At
+  prompt.context.ERC721 = ERC721At
+  prompt.context.token = token
   prompt.context.local = database
-
-  // Every prompt function following this line should be bound to a button. The above are "low level" calls for the more advanced user. All functions should take string argumetns since they'er expected to be parsed from an input box or from the repl. The conversion to BNs should be done under the hood.
-
-  // Skip ahead time for finalizing exits in local tests
-  prompt.context.timeskip = async () => await increaseTime(web3, 8 * 24 * 3600)
-
-  // Refresh the user's state
-  prompt.context.refresh = async () => await user.refreshAsync()
-
-  // Set some convenience addresses
   prompt.context.addressbook = addressbook
 
-  // Initialize a demo erc721 token
-  if (!(erc721 === undefined)) {
-    const token = myERC721(erc721).instance
-    prompt.context.token = token
-    // Easy function to deposit coins to plasma
-    prompt.context.deposit = async (uid: string) => {
-      return await token.safeTransferFrom([addressbook.self, addressbook.plasmaAddress, new BN(uid)])
-    }
-  }
+  // These are the functions that should be made available to the user in the UI
 
-  // Get all deposits, filtered by the user's address.
-  if (startBlock !== undefined) {
-    prompt.context.deposits = async () => await user.getDepositEvents(startBlock, false)
-  }
+  const user = new User(entity, database, web3, startBlock, addressbook, token)
+  prompt.context.timeskip = user.timeskip
+  prompt.context.refresh = user.refresh
+  prompt.context.deposit = user.deposit
+  prompt.context.transfer = user.transfer
+  prompt.context.exit = user.exit
+  prompt.context.finalize = user.finalizeExit
+  prompt.context.withdraw = user.withdraw
+  prompt.context.withdrawBonds = user.withdrawBonds
+  prompt.context.deposits = user.deposits
+  prompt.context.getCoin = user.coin
 
-  // TODO Check how prevBlockNum is saved and how exitBlockNum is saved. Keep every number in the same format in the local stoage
-  // Exiting a coin by specifying the slot. Finding the block numbers is done under the hood.
-  prompt.context.exit = async (slot: string) => {
-    const coinData: any[] = database.getCoin(slot)
-    // Search for the latest transaction in the coin's history, O(N)
-    let max = coinData[0].block
-    let tx = coinData[0].tx
-    for (let i in coinData) {
-      const coin = coinData[i]
-      if (coin.blockNumber > max) {
-        max = coin.block
-        tx = coin.tx
-      }
-    }
-    return await user.startExitAsync({
-      slot: new BN(slot, 16),
-      prevBlockNum: new BN(tx.prevBlockNum, 16), // prev block is in 0x FIXIFIXFIX
-      exitBlockNum: new BN(max) // max is in decimal
-    })
-  }
-
-  // Transfer a coin by specifyign the
-  prompt.context.transfer = async (slot: string, newOwner: string) => {
-    const coinData: IDatabaseCoin[] = database.getCoin(slot)
-    // Search for the latest transaction in the coin's history, O(N)
-    let max = coinData[0].blockNumber
-    for (let i in coinData) {
-      const coin = coinData[i]
-      if (coin.blockNumber > max) {
-        max = coin.blockNumber
-      }
-    }
-    return await user.transferTokenAsync({
-      slot: new BN(slot, 16),
-      prevBlockNum: new BN(max, 16),
-      denomination: 1,
-      newOwner: newOwner
-    })
-  }
-
-  prompt.context.withdraw = async (slot: string) => {
-      return await await user.withdrawAsync(new BN(slot, 16))
-  }
-
-  prompt.context.withdrawBonds = async () => {
-      return await user.withdrawBondsAsync()
-  }
-
+  // Apply the async/await functionality on the repl
   transform(prompt)
 }
---
+++ src/modules/user.ts
@@ -0,0 +1,109 @@
+import BN from 'bn.js'
+import { PlasmaDB, Entity } from 'loom-js'
+import { increaseTime } from './ganache-helpers'
+import Web3 from 'web3'
+
+// User friendly wrapper for all Entity related functions, taking advantage of the database
+export class User {
+  private _user: Entity
+  private _database: PlasmaDB
+  private _web3: Web3
+  private _startBlock: BN | undefined
+  private _addressbook: any
+  private _token: any | undefined
+
+  constructor(
+    user: Entity,
+    database: PlasmaDB,
+    web3: Web3,
+    addressbook: any,
+    token?: any,
+    startBlock?: BN
+  ) {
+    this._user = user
+    this._database = database
+    this._startBlock = startBlock
+    this._web3 = web3
+    this._addressbook = addressbook
+    this._token = token
+  }
+
+  // TODO Check how prevBlockNum is saved and how exitBlockNum is saved. Keep every number in the same format in the local stoage
+  // Exiting a coin by specifying the slot. Finding the block numbers is done under the hood.
+  async exit(slot: string) {
+    const coinData: any[] = this._database.getCoin(slot)
+    // Search for the latest transaction in the coin's history, O(N)
+    let max = coinData[0].block
+    let tx = coinData[0].tx
+    for (let i in coinData) {
+      const coin = coinData[i]
+      if (coin.blockNumber > max) {
+        max = coin.block
+        tx = coin.tx
+      }
+    }
+    return await this._user.startExitAsync({
+      slot: new BN(slot, 16),
+      prevBlockNum: new BN(tx.prevBlockNum, 16), // prev block is in 0x FIXIFIXFIX
+      exitBlockNum: new BN(max) // max is in decimal
+    })
+  }
+
+  // Transfer a coin by specifying slot & new owner
+  async transfer(slot: string, newOwner: string) {
+    const coinData: any[] = this._database.getCoin(slot)
+    // Search for the latest transaction in the coin's history, O(N)
+    let max = coinData[0].blockNumber
+    for (let i in coinData) {
+      const coin = coinData[i]
+      if (coin.blockNumber > max) {
+        max = coin.blockNumber
+      }
+    }
+    return await this._user.transferTokenAsync({
+      slot: new BN(slot, 16),
+      prevBlockNum: new BN(max, 16),
+      denomination: 1,
+      newOwner: newOwner
+    })
+  }
+
+  async finalizeExit(slot: string) {
+    await this._user.plasmaCashContract.finalizeExit(new BN(slot, 16))
+  }
+
+  async withdraw(slot: string) {
+    await this._user.withdrawAsync(new BN(slot, 16))
+  }
+
+  async withdrawBonds() {
+    await this._user.withdrawBondsAsync()
+  }
+
+  async coin(slot: string) {
+    await this._user.getPlasmaCoinAsync(new BN(slot, 16))
+  }
+
+  // Get all deposits, filtered by the user's address.
+  async deposits() {
+    await this._user.getDepositEvents(this._startBlock || new BN(0), false)
+  }
+
+  async refresh() {
+    await this._user.refreshAsync()
+  }
+
+  // Skip ahead time for finalizing exits in local tests
+  async timeskip() {
+    await increaseTime(this._web3, 8 * 24 * 3600)
+  }
+
+  // Initialize a demo erc721 token
+  async deposit(uid: string) {
+    await this._token.safeTransferFrom([
+      this._addressbook.self,
+      this._addressbook.plasmaAddress,
+      new BN(uid)
+    ])
+  }
+}
